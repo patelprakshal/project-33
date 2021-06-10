c8e05f93817ea15c9b13d037cef25692b23a8411
patelprakshal <74961658+patelprakshal@users.noreply.github.com>
2021-06-10 17:24:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var snowImag3;
+
+function preload(){
+  bg =loadImage("snow3.jpg");
+  
+ }
+
+
+function setup() {
+ 
+ createCanvas(1500,700);
+
+  
+}
+
+function draw() {
+ background(bg);
+
+}
